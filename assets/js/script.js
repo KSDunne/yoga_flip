dbbff3f548535291ed0cd9254965dce48c75e1db
KSDunne <77549699+KSDunne@users.noreply.github.com>
2023-09-13 13:59:30 +0100

    Add comments to script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 // script.js
 const memory_cards = document.querySelectorAll(".cards");
 
+// flip card function
 function flipCard() {
   this.classList.toggle("flip");
 }
 
+// event listener which calls the flip card function
 memory_cards.forEach((card) => card.addEventListener("click", flipCard));
