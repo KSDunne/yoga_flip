5ed07750c86a93d5310cd52d3b0df20eeaa26d97
KSDunne <77549699+KSDunne@users.noreply.github.com>
2023-09-13 16:11:00 +0100

    Add matching logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,41 @@
 // script.js
 const memory_cards = document.querySelectorAll(".cards");
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 // flip card function
 function flipCard() {
   this.classList.toggle("flip");
+  this.classList.add("flip");
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
+
+  secondCard = this;
+  hasFlippedCard = false;
+
+  checkForMatch();
+}
+
+function checkForMatch() {
+  let isMatch = firstCard.dataset.image === secondCard.dataset.image;
+  isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+}
+
+function unflipCards() {
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+  }, 1500);
 }
 
 // event listener which calls the 'flip card function' when a card is clicked
